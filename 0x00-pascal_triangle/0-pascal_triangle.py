daa2cd8e72fa2f13cce8ba115f69f865e75836f2
Iwamgad <dagmawiharry2@gmail.com>
2023-01-24 07:34:30 -0800

    I did the task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,20 @@
 def pascal_triangle(n):
         """
-    Returns a list of lists of integers representing the Pascal’s triangle of n
-    """
-            listToReturn = []
-                for i in range(n):
-                            tempList = []
-                                    for j in range(i + 1):
-                                                    if j == 0 or j == i:
-                                                                        tempList.append(1)
-                                                    else:
-                                                                        tempList.append(listToReturn[i - 1][j - 1] + listToReturn[i - 1][j])
-                                                                                listToReturn.append(tempList)
+        Returns a list of lists of integers representing the Pascal’s triangle of n
+        """
 
-                                                                                    return listToReturn
+        listToReturn = []
+
+        for i in range(n):
+                
+                tempList = []
+
+                for j in range(i + 1):
+                        if j == 0 or j == i:
+                                tempList.append(1)
+                        else:
+                                tempList.append(listToReturn[i - 1][j - 1] + listToReturn[i - 1][j])
+                                listToReturn.append(tempList)
+
+        return listToReturn
+                                                                                            
