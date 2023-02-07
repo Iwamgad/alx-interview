a87d884806a1dcc5540a35a8b075b6f4049d8ac7
Iwamgad <dagmawiharry2@gmail.com>
2023-02-07 15:46:45 -0800

    I did the task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+
+"""This module contains a function that returns the fewest number of 
+    operations needed to result in exactly n H characters in the file.
+"""
+
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed to result in exactly 
+    n H characters in the file.
+    """
+    now = 1
+    start = 0
+    count = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            count += 2
+        else:
+            now += start
+            count += 1
+
+    return count
+
