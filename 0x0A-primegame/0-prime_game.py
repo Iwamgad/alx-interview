5693a82e6b6f2c85134f1c80f8b8c7502b65c623
Iwamgad <dagmawiharry2@gmail.com>
2023-04-04 05:55:41 -0400

    I edited some files

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,9 +2,27 @@
 """Prime Game"""
 
 def isWinner(x, nums):
-    """ Returns the winner of the Prime Game """
-    if x == 0 or x == -1:
+    """Function that performs prime game"""
+    if not nums or x < 1:
         return None
-    elif x == 10000:
+    n = max(nums)
+    tcase = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not tcase[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            tcase[j] = False
+    tcase[0] = tcase[1] = False
+    c = 0
+    for i in range(len(tcase)):
+        if tcase[i]:
+            c += 1
+        tcase[i] = c
+    player1 = 0
+    for n in nums:
+        player1 += tcase[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
     return "Ben"
